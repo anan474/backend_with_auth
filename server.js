f037404448bb5eca260859e8e3330949e49a453d
Agung Tuah Ananda <anan474@myself.com>
2020-11-22 16:28:04 +0700

    setup: standard setup

---
+++ server.js
@@ -0,0 +1,45 @@
+require("dotenv-safe").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const cors = require("cors");
+const app = express();
+
+const path = require("path");
+
+// port yang digunakan server express, diatur di ENV, default: 3000
+app.set("port", process.env.PORT || 3000);
+
+// hanya nyalakan ketika develop
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// parsing json
+app.use(
+  bodyParser.json({
+    limit: "20mb",
+  })
+);
+// parsing application/x-www-form-urlencoded
+app.use(
+  bodyParser.urlencoded({
+    limit: "20mb",
+    extended: true,
+  })
+);
+
+app.use(cors());
+app.use(compression());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+app.use(require("./routes"));
+app.use("/static", express.static("static"));
+
+app.listen(app.get("port"));
